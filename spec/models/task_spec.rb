456106d24ef369995a6a845d22d2af7fa3f40c06
Steve Rowley <srowley@gmail.com>
2013-12-26 12:55:41 +0000

    task spec cleanup

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,33 +3,39 @@ require 'spec_helper'
 describe Task do
 
   before(:each) do
-    @valid_task = FactoryGirl.build(:task)
+    @valid_task = build(:task)
   end
 
-  [:owner, :agency, :facility, :due_date].each do |attribute|
-    it "should be invalid without an " + attribute.to_s do
-      expect(FactoryGirl.build(:task, attribute => nil)).to_not be_valid
+  context 'is not valid when' do
+
+    [:owner, :agency, :facility, :due_date].each do |attribute|
+      it "the value of " + attribute.to_s + " is nil" do
+        expect(build(:task, attribute => nil)).to_not be_valid
+      end
+    end
+
+    it 'the value of the completed date is not valid date' do
+      expect(build(:task, completed_date: "foo")).to_not be_valid
+    end
+
+    it 'the value of the due date is not a valid date' do
+      expect(build(:task, due_date: "foo")).to_not be_valid
+    end
+
+    it 'the completed date is after the current date' do
+      expect(build(:task, completed_date: Time.now + 1.day)).to_not be_valid
+    end
+
+  end
+
+  context 'is valid when' do
+
+    it 'all fields are completed' do
+      expect(@valid_task).to be_valid
+    end
+
+    it 'the completed date is blank' do
+      expect(build(:task, completed_date: nil)).to be_valid
     end
   end
-
-  it "should be invalid if the value of the completed date is not valid date" do
-    expect(FactoryGirl.build(:task, completed_date: "foo")).to_not be_valid
-  end
-
-  it "should be invalid if the value of the due date is not a valid date" do
-    expect(FactoryGirl.build(:task, due_date: "foo")).to_not be_valid
-  end
-
-  it "should be valid if all fields are completed" do
-    expect(@valid_task).to be_valid
-  end
-
-  it "should be valid even without a completed date" do
-    expect(FactoryGirl.build(:task, completed_date: nil)).to be_valid
-  end
-
-  it "should be invalid if the completed date is after the current date" do
-    expect(FactoryGirl.build(:task, completed_date: Time.now + 1.day)).to_not be_valid
-  end
-
 end
