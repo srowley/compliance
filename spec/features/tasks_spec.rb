f3ee399f8714fc51cc2d06554598b169eeff65a9
Steve Rowley <srowley@gmail.com>
2013-12-27 01:12:04 +0000

    pending scenarios written

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+feature 'Manage task records' do
+
+  scenario 'view a record'
+  
+  scenario 'add a new record'
+
+  scenario 'edit a record'
+
+  scenario 'delete a record'
+  
+end
