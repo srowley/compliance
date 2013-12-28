5a7d7239f080048976bcc6d04afb31dafc121b26
Steve Rowley <srowley@gmail.com>
2013-12-28 12:02:10 +0000

    crud feature specs work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,10 +12,15 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to @task
+      flash[:notice] = 'Record updated successfully.'
+      redirect_to task_path(@task) 
     else
       flash[:alert] = 'Update invalid. Record not saved.'
       render 'edit'
--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 feature 'Manage task records' do
 
+  def fill_in_form(modifier)
+    fill_in 'task_facility', with: "#{modifier} Facility"                      
+    fill_in 'task_agency', with: "#{modifier} Agency"      
+    fill_in 'task_owner', with: 'Stefan'                                     
+    fill_in 'task_description', with: 'New Task Description'                 
+    select '2015', from: 'task_due_date_1i'                                  
+    select 'January', from: 'task_due_date_2i'                               
+    select '1', from: 'task_due_date_3i'                                     
+    click_button 'Submit'
+  end
+
   background do
-    first_task = create(:task, description: "First task")
-    second_task = create(:task, description: "Second task")
+    @first_task = create(:task, description: "First task")
+    @second_task = create(:task, description: "Second task")
   end
 
   scenario 'view all records' do
@@ -15,23 +26,25 @@ feature 'Manage task records' do
   
   scenario 'add a new record' do
     visit new_task_path
-    puts page.body
-    expect{
-      fill_in 'task_facility', with: 'New Facility'
-      fill_in 'task_agency', with: 'New Agency'
-      fill_in 'task_owner', with: 'Stefan'
-      fill_in 'task_description', with: 'New Task Description'
-      select '2015', from: 'task_due_date_1i'
-      select 'January', from: 'task_due_date_2i'
-      select '1', from: 'task_due_date_3i'
-      click_button 'Add Task'
-      }.to change(Task, :count).by(1)
+    expect{ fill_in_form("New") }.to change(Task, :count).by(1)
     expect(page).to have_content 'Record saved successfully.'
     expect(page).to have_content 'New Facility' 
   end
 
-  scenario 'edit a record'
+  scenario 'edit a record' do
+    visit edit_task_path(@first_task)
+    expect{ fill_in_form("Updated") }.to change(Task, :count).by(0)
+    expect(page).to have_content 'Record updated successfully.'    
+    expect(page).to have_content 'Updated Facility'
+  end
 
-  scenario 'delete a record'
+  scenario 'delete a record' do
+    visit tasks_path
+    puts page.body
+    expect{ find("a[href='/tasks/1'][data-method='delete']").click
+      }.to change(Task, :count).by(-1)
+    expect(page).to_not have_content 'First task'
+    puts page.body
+  end
 
 end
