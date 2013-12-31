317fc58f27504e02890569ca85814b1757c2cbd3
Steve Rowley <srowley@gmail.com>
2013-12-31 19:59:45 +0000

    moved sorcery test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,18 +59,4 @@ RSpec.configure do |config|
   
 end
 
-# login_user for feature specs
-# thanks https://gist.github.com/austenito/2359120
-module Sorcery
-  module TestHelpers
-    module Rails
-      def login_user_post(user, password)
-        page.driver.post(sessions_url, { username: user, password: password}) 
-      end
-      
-      def logout_user_get
-        page.driver.get(logout_url)
-      end
-    end
-  end
-end
+
