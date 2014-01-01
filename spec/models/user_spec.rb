50f215a39be8edbf75ee644d1e705ba5a9f2bf34
Steve Rowley <srowley@gmail.com>
2014-01-01 04:42:17 +0000

    rid of useless example group

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,30 +2,28 @@ require 'spec_helper'
 
 describe User do
   
-  describe "validations" do
-    before(:each) do
-      @valid_user = build(:user)
-    end
+  before(:each) do
+    @valid_user = build(:user)
+  end
     
-    context 'is not valid when' do
+  context 'is not valid when' do
 
-      [:username, :email, :user_first_name, :user_last_name].each do |attribute|
-        it "the value of #{attribute} is nil" do
-          expect(build(:user, attribute => nil)).to_not be_valid
-        end
-      end
-
-      it "the username is not unique" do
-        create(:user)
-        expect(@valid_user).to_not be_valid
+    [:username, :email, :user_first_name, :user_last_name].each do |attribute|
+      it "the value of #{attribute} is nil" do
+        expect(build(:user, attribute => nil)).to_not be_valid
       end
     end
+
+    it "the username is not unique" do
+      create(:user)
+      expect(@valid_user).to_not be_valid
+    end
+  end
     
-    context 'is valid when' do
+  context 'is valid when' do
       
-      it 'all fields are completed' do
-        expect(@valid_user).to be_valid
-      end
+    it 'all fields are completed' do
+      expect(@valid_user).to be_valid
     end
   end
 end
