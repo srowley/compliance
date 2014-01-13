596c76453751aaa362f0f0d978fda728fce1a62e
Steve Rowley <srowley@gmail.com>
2014-01-13 12:29:13 +0000

    permissions implemented on task owner and non-owner

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,6 +1,14 @@
 class TaskPolicy < ApplicationPolicy
 
+  def show?
+    user.has_role? :owner, record 
+  end
+  
   def update?
     user.has_role? :owner, record
+  end
+
+  def destroy?
+    user.has_role? :editor
   end
 end
--- spec/policies/task_policy_spec.rb
+++ spec/policies/task_policy_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
 describe TaskPolicy do
-  subject { TaskPolicy.new(owner, task) }
 
   let(:owner) { create(:user) }
+  let(:non_owner) { create(:user, username: 'jane') }
   let(:task) { create(:task_with_owner, user: owner) }
 
   context "for the task owner" do
+    subject { TaskPolicy.new(owner, task) }
+
     it { should permit(:show)    }
     it { should permit(:create)  }
     it { should permit(:new)     }
     it { should permit(:update)  }
     it { should permit(:edit)    }
-    it { should permit(:destroy) }
+    it { should_not permit(:destroy) }
+  end
+  
+  context "for a task non-owner" do
+    subject { TaskPolicy.new(non_owner, task) }
+
+    it { should permit(:create) }
+    it { should permit(:new) }
+    it { should_not permit(:show) }
+    it { should_not permit(:update) }
+    it { should_not permit(:edit) }
+    it { should_not permit(:destroy) }
   end
 end
