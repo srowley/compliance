de7993a0337f343849b2e53ee5ce977e2a43b55f
Steve Rowley <srowley@gmail.com>
2014-01-16 11:58:51 +0000

    see actions you can actually do

--- spec/views/tasks/tasks_index_spec.rb
+++ spec/views/tasks/tasks_index_spec.rb
@@ -8,48 +8,106 @@ describe 'tasks/index.html.haml' do
 
   before(:each) do
     @user = create(:user)
+    view.stub(:current_user).and_return(@user)
+    view.stub(:policy).and_return double(:policy, update?: true, destroy?: false)
     @first_task = create(:task_with_owner, description: 'First task', user: @user)
     @second_task = create(:task_with_owner, description: 'Second task', user: @user)
     tasks = Task.all
     assign :tasks, tasks
-    render
   end
 
-  it "has the page heading 'All Tasks'" do
-    expect_selector("h1", text: 'All Tasks')
+  describe "the page shell" do
+    before(:each) do
+      render
+    end
+
+    it "has the page heading 'All Tasks'" do
+      expect_selector("h1", text: 'All Tasks')
+    end
+
+    it 'has a link to create a new record' do
+      expect_selector("a[href='/tasks/new']", text: 'New Task')
+    end
   end
+  
+  describe "the export link" do
+    before(:each) do
+      render
+    end
+
+    context "when the results are not filtered" do
+      it "has no filter parameters appended" do
+        expect_selector("a[href='/tasks/export.csv']")
+      end
+    end
+
+    context "when the results are filtered (no test, see comments)" do
+      it "has filter parameters appended" do
+        # can't figure out how to pass params at render time
+        # expect_selector("a[href*='/tasks/export.csv?filter']")
+      end
+    end
+  end    
 
   describe 'the task list table' do
+    before(:each) do
+        @user = create(:user)
+        view.stub(:current_user).and_return(@user)
+        view.stub(:policy).and_return double(:policy, update?: true, destroy?: false)
+        @first_task = create(:task_with_owner, description: 'First task', user: @user)
+        @second_task = create(:task_with_owner, description: 'Second task', user: @user)
+        tasks = Task.all
+        assign :tasks, tasks
+      end
+
     it 'displays all tasks' do
+      render
       expect_selector("table tr td", text: 'First task')
       expect_selector("table tr td", text: 'Second task')
     end
-  end
 
-  it 'has a link to create a new record' do
-    expect_selector("a[href='/tasks/new']", text: 'New Task')
-  end
+    describe 'each row in the task list table' do
 
-  describe 'each row in the task list table' do
+      it 'has a link to show the full record for each task' do
+        render
+        expect_selector("table tr td", text: 'First task')
+        expect_selector("table tr td", text: 'ISO')
+        expect_selector("table tr td", text: 'Power Center 1')
+        expect_selector("table tr td", text: 'Blow, Joe')
+      end
 
-    it 'has a link to show the full record for each task' do
-      expect_selector("table tr td a[href='/tasks/#{@first_task.id}']", text: 'First task')
-      expect_selector("table tr td", text: 'ISO')
-      expect_selector("table tr td", text: 'Power Center 1')
-      expect_selector("table tr td", text: 'Blow, Joe')
-    end
+      context 'if user has permission to edit the task'  do
+        it 'has a link to edit the record' do
+          render
+          expect_selector("table tr td a[href='/tasks/#{@first_task.id}/edit']", text: 'Edit')
+        end
+      end
 
-    it 'has a link to edit the record for that task' do
-      expect_selector("table tr td a[href='/tasks/#{@first_task.id}/edit']", text: 'Edit')
-    end
+      context 'if user does not have permission to edit the record' do
+        it 'has a link to view the record' do
+          view.stub(:policy).and_return double(:policy, update?: false)
+          render
+          expect_selector("table tr td a[href='/tasks/#{@first_task.id}']", text: 'View')
+        end
+      end
 
-    it 'has a link to delete the record for that task' do
-      expect_selector("table tr td a[href='/tasks/#{@first_task.id}']", text: 'Delete')
+      context 'if user has permission to destroy the record' do
+        it 'has a link to delete the record' do
+          editor = create(:editor)
+          view.stub(:current_user).and_return(editor)
+          view.stub(:policy).and_return double(:policy, update?: true, destroy?: true)
+          render
+          expect_selector("table tr td a[href='/tasks/#{@first_task.id}']", text: 'Delete')
+        end
+      end
     end
   end
   
   describe "the filter form" do
-     
+    before(:each) do
+      render
+    end
+
     it "has a field to specify the due date to display" do
       expect_selector("form input[name='filter[due_date]']")
     end
@@ -64,21 +122,6 @@ describe 'tasks/index.html.haml' do
 
     it "has a button to filter the task list table" do
       expect_selector("form input[type='submit'][value='Filter Results']")
-    end
-  end
-  
-  describe "the export link" do
-    context "when the results are not filtered" do
-      it "has no filter parameters appended" do
-        expect_selector("a[href='/tasks/export.csv']")
-      end
-    end
-    
-    context "when the results are filtered (no test, see comments)" do
-      it "has filter parameters appended" do
-        # can't figure out how to pass params at render time
-        # expect_selector("a[href*='/tasks/export.csv?filter']")
-      end
     end
   end
 end
